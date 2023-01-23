0a2ed6668c594dfb48afd31d19720d36b9668aeb
deepak271 <dy195360@gmail.com>
2023-01-23 21:54:11 +0530

    api call feature added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const connectDb = require('./db');
 const express = require('express');
 const bodyparser = require('body-parser');
 const routes = require('./controller/routes/Router');
+const cors = require('cors');
 require('dotenv').config();
 
 connectDb();
@@ -12,6 +13,7 @@ const port = process.env.PORT || 8080;
 //Routes & middlewares
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true}));
+app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 app.use('/api/auth',routes);
